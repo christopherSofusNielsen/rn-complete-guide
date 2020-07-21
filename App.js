2dfa0350b04a03d2ecdb916f2202cf98e1814b81
Christopher Nielsen <c.consult.christopher@gmail.com>
2020-07-21 13:37:56 +0200

    finished app

--- App.js
+++ App.js
@@ -1,29 +1,57 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput, Button, FlatList } from "react-native";
+
+//Components
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [couseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const onAddGoalHandler = (enteredGoal) => {
+    setCourseGoals((currentState) => [
+      ...currentState,
+      { id: Math.random().toString(), value: enteredGoal },
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals((currentState) => {
+      return currentState.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  const cancelIsAddMode = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={{ padding: 30 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Course goal"
-          style={{
-            borderBottomColor: "black",
-            borderBottomWidth: 1,
-            width: "80%",
-          }}
-        />
-        <Button title="ADD" />
-      </View>
-      <View></View>
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={onAddGoalHandler}
+        onCancel={cancelIsAddMode}
+      />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={couseGoals}
+        renderItem={({ item }) => (
+          <GoalItem
+            id={item.id}
+            onDelete={removeGoalHandler}
+            title={item.value}
+          />
+        )}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+});
